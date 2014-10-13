4757630f2213df4b0a1efe8c64386d542635fc10
KaiserEMP <kaidogta3@hotmail.com>
2014-10-13 15:31:10 +0300

    customized project settings added products_view

---
+++ controllers/products.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaiser
+ * Date: 13.10.14
+ * Time: 11:03
+ */
+
+class products extends Controller {
+    function index() {
+
+}
+}
---
+++ views/products/products_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaiser
+ * Date: 13.10.14
+ * Time: 15:28
+ */
