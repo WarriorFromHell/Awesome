1da484be26c32c7bce10174c940faa3dad2d2d75
Henno Taht <henno.taht@khk.ee>
2014-10-20 15:41:32 +0300

    Reintroduce halo

--- views/products/products_index.php
+++ views/products/products_index.php
@@ -4,33 +4,10 @@
 
         <p><?= $product['product_text'] ?></p>
 
-       <!--
-        <div>
-            <span class="badge badge-success">
-
-                <?= $post['post_created'] ?>
-
-            </span>
-
-            <div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span
-                    class="label">blog</span> <span class="label">personal</span></div>
-        </div>
-        -->
-
-
-        <? foreach ($tags[$post['post_id']] as $tag): ?>
+    <? foreach ($tags[$post['post_id']] as $tag): ?>
             <a href="tags/view/<?= $tag['tag_name'] ?>">
                 <span class="label" style="background-color: #5bc0de"><?= $tag ?></span>
             </a> <? endforeach ?>
 
     </div>
 <? endforeach ?>
-
-
-<!--
-<form method="post">
-    <input name="data[post_text]" type="text"placeholder="pealkiri"/><br/><br/>
-    <textarea name="data[post_text]" id="" cols="30" rows="10" placeholder="Sisu"></textarea><br/>
-    <button class="btn  btn-default">Sisesta</button>
-</form>
--->
