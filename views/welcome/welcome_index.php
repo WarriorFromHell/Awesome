9d30a160baad8a771ae90762ba844d226a4d3043
KaiserEMP <kaidogta3@hotmail.com>
2014-12-11 13:41:00 +0200

    Implement bank payments Todo: make orders private again buy page onload welcome discount product  ahref

--- views/welcome/welcome_index.php
+++ views/welcome/welcome_index.php
@@ -10,12 +10,17 @@
 
     <div class="row">
         <? foreach ($discounts as $discount): ?>
-            <div class="well pull-left" style="width:290px;margin:15px">
-                <h2><?= $discount['product_name'] ?></h2>
 
-                <p><?= $discount['product_description'] ?></p><img src="<?= getDataURI($discount['product_image']) ?>"
-                                                                   alt=""/>
-            </div>
+            <a href="./products/<?= $discount['product_id'] ?>">
+
+                <div class="well pull-left" style="width:290px;margin:15px">
+                    <h2><?= $discount['product_name'] ?></h2>
+
+                    <p><?= $discount['product_description'] ?></p><img src="<?= getDataURI($discount['product_image']) ?>"
+                                                                       alt=""/>
+                </div>
+
+            </a>
         <? endforeach ?>
     </div>
 </div>
